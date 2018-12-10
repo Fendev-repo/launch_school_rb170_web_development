1dcbaf04978e28874e896e08ca509ccb5eab52a9
FenDev <fendev@protonmail.com>
2018-12-10 17:22:32 +1100

    correct some rubocop cops

--- 02_working_with_sinatra/03_code_challenge_dynamic_directory_index/dynamic_index_app.rb
+++ 02_working_with_sinatra/03_code_challenge_dynamic_directory_index/dynamic_index_app.rb
@@ -1,12 +1,12 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
-get "/" do
+get '/' do
   @all_files = Dir.entries('public')
   @parameters = params
 
   @all_files.reverse! if @parameters[:sort] == 'desc'
-		
+
   erb :main
 end
